856951a49980b3300c70afd70ce6c8501ff73901
Kristen Spencer <kristen@kristenspencer.ca>
2018-01-15 17:29:48 -0500

    adds styling

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
+// images
+import upvote from '../images/upvote.png';
+import downvote from '../images/downvote.png';
+
 const ListItem = ( { itemInfo, updateVote } ) => {
  
-  return (
-    <li className="list__item">
+  return <li className="list__item">
       {itemInfo.item}
       <div className="item__scoreCard">
         <span className="scoreCard__score">{itemInfo.score}</span>
-        <button onClick={() => updateVote(itemInfo._id, itemInfo.score, 1)}><span role="img" aria-label="thumbs up">👍</span>Upvote</button>
-        <button onClick={() => updateVote(itemInfo._id, itemInfo.score, -1)}>Downvote<span role="img" aria-label="thumbs down">👎</span></button>
+        <button onClick={() => updateVote(itemInfo._id, itemInfo.score, 1)}>
+          <img src={upvote} alt="thumbs up" />
+          Upvote
+        </button>
+        <button onClick={() => updateVote(itemInfo._id, itemInfo.score, -1)}>
+          <img src={downvote} alt="thumbs down" />Downvote
+        </button>
       </div>
-    </li>
-  )
+    </li>;
 }
 
 export default ListItem;
